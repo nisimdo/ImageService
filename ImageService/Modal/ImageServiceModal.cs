3d2f89d069962aab634b8e4244405063c413a37e
Dor Nisim <Dor Nisim@DESKTOP-H2OSS5Q>
2018-02-27 14:12:10 +0200

    Removing X

--- ImageService/Modal/ImageServiceModal.cs
+++ ImageService/Modal/ImageServiceModal.cs
@@ -12,8 +12,6 @@ namespace ImageService.Modal
 {
     public class ImageServiceModal : IImageServiceModal
     {
-        // Hello
-        int x;
         #region Members
         private string m_OutputFolder;            // The Output Folder
         private int m_thumbnailSize;              // The Size Of The Thumbnail Size
